d7e3e05f801c688fd444ee2bfab8852d76ab684d
Temitope <temmieayodele@gmail.com>
2020-03-27 00:38:11 +0100

    added new images and style to each reviewdetail card

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { globalStyles } from "../styles/global";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
 import Card from "../shared/card";
 
 export default function reviewDetails({ navigation }) {
   // const pressHandler = () => {
   //   navigation.goBack()
   // };
+  const rating = navigation.getParam("rating");
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{navigation.getParam("title")}</Text>
         <Text>{navigation.getParam("body")}</Text>
-        <Text>{navigation.getParam("rating")}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
 
       {/* <Button title="Back to Home" onPress={pressHandler} /> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#eee"
+  }
+});
