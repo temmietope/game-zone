450a9434b08dd022063c12222d75f1eb60310572
Temitope <temmieayodele@gmail.com>
2020-03-26 19:56:39 +0100

    navigating around

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
+import { NavigationEvents } from "react-navigation";
 
-export default function reviewDetails() {
+export default function reviewDetails({ navigation }) {
+  const pressHandler = () => {
+    navigation.goBack();
+  };
   return (
     <View style={globalStyles.container}>
       <Text>Review details Screen</Text>
+      <Button title="Back to Home" onPress={pressHandler} />
     </View>
   );
 }
