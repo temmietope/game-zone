0d11a65963630b03fcf92561cb8409fd779499ff
Temitope <temmieayodele@gmail.com>
2020-03-26 19:07:33 +0100

    stack-navigation

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import Navigator from "./routes/homeStack";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf")
+  });
+
+export default function App() {
+  const [fontsLoaded, setFontLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <Navigator />;
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontLoaded(true)} />
+    );
+  }
+}
---
+++ screens/reviewDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function reviewDetails() {
+  return (
+    <View style={globalStyles.container}>
+      <Text>Review details Screen</Text>
+    </View>
+  );
+}
